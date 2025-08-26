48561504bb45fb0e8eb7a8da285e8ea6e1248db4
mehboobmehi <advocatemehboobrahi@gmail.com>
2025-08-27 04:23:51 +0500

    new code added

---
+++ script.js
@@ -0,0 +1,9 @@
+// let age = 15;
+
+// if (age > 18){
+//     console.log(`"You're ${age} old you may enter the club "`)
+// } else if ( age === 18){
+//     console.log(`"you jusr turned ${age} you may ente welocme"`)
+// } else {
+//     console.log(`"You are ${age} very young cant enter"`)
+// }
