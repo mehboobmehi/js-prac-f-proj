037db98b94a106eaa105a8d692114ab1e98a6224
mehboobmehi <advocatemehboobrahi@gmail.com>
2025-08-28 02:52:17 +0500

    I added some new code

--- script.js
+++ script.js
@@ -7,3 +7,49 @@
 // } else {
 //     console.log(`"You are ${age} very young cant enter"`)
 // }
+
+// let age = 18;
+// let hasId = false;
+
+// if (age >= 18 && hasId === true){
+//     console.log('you may enter the club')
+// } else {
+//     console.log('You cant enter the gate')
+// }
+
+// let itsRaining = false;
+// itsRaining ? console.log("It's raining ") : console.log("It's not raining")
+
+// let itsRaising = true;
+// let str = itsRaising?  "its raising" : "its not raising"
+// console.log(str);
+
+// let age = 19;
+// let hasId = true;
+// let str = age >= 18 || hasId===false? "You may enter" : "you can't enter"
+// console.log(str);
+
+// let loggedIn = true;
+// let hasMembership = false;
+// let str = loggedIn ===true || hasMembership ===true ? "Show the video" : "don't show the video"
+// console.log(str)
+
+// let counter = 1;
+// while (counter <= 7) {
+//     console.log(counter);
+//     counter  = counter +1 ;
+
+// }
+// console.log("while loop finished")
+// for (let i = 0; i < 5; i++){
+//     console.log(i)
+// }
+// for(let i=1; i<=15; i++){
+//     console.log(i)
+// }
+
+
+let sry = "ASAP Frontend";
+for (let i=0; i<sry.length; i++){
+    console.log(sry[i])
+}
